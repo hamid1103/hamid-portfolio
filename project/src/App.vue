5ce296aeca7d058a32219f2d29c3fc92b137c611
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-02-05 17:38:09 +0100

    wurh

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,23 +1,39 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import SideBar from './components/SideBar.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    </div>-->
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="sidebarcont">
+      <side-bar/>
+    </div>
+
+    <div class="contained">
+      <TheWelcome />
+    </div>
+
+
+  </header>
 </template>
 
 <style scoped>
+
+.contained{
+
+}
+
+.sidebarcont{
+  margin-right: 3em;
+}
+
 header {
   line-height: 1.5;
 }
@@ -30,8 +46,13 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
+    flex-direction: row;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .contained{
+
   }
 
   .logo {
---
+++ project/src/components/SideBar.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="sidebar">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "SideBar"
+}
+</script>
+
+<style scoped>
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .sidebar{
+    background-color: #2f2f2f;
+    height: 75vh;
+    width: 20vh;
+    display: flex;
+    position: fixed;
+  }
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
